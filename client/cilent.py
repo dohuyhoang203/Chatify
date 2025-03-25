555e093b5333bcfda3b6b33996f1e2412bbc9086
Do_Huy_Hoang <hoangdo18022003@gmail.com>
2025-03-25 16:11:58 +0700

    Delete all files

--- client/cilent.py
+++
@@ -1,8 +0,0 @@
-import uuid
-import socketio
-
-if __name__ == '__main__':
-    sio = socketio.Client()
-    sio.connect('http://192.168.2.14:5002')
-    sio.emit('check-in', {'user_id': str(uuid.uuid1())})
-    sio.wait()
